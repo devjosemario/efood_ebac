cee6cadaa16d1921ef1df5b344e80043de7017b8
jose <gamerick227@gmail.com>
2026-02-16 15:52:10 -0300

    finalizado

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import RestaurantList from '../../RestaurantList'
+import Hero from '../../Hero'
+import { useGetRestaurantsQuery } from '../../services/api'
+
+const Home = () => {
+  const { data: restaurantes } = useGetRestaurantsQuery()
+
+  if (!restaurantes) {
+    return <h3>Carregando restaurantes...</h3>
+  }
+
+  return (
+    <>
+      <Hero />
+      <RestaurantList restaurants={restaurantes} />
+    </>
+  )
+}
+
+export default Home
